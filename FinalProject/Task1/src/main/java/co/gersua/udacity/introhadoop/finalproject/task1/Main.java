848336ce5667c3e28b17974e794a5ce41c149da9
German Alonso Suarez Guerrero <gersua@GERMANs-MacBook-Pro.local>
2016-03-30 00:12:26 -0500

    Task1 skelleton for Final

---
+++ FinalProject/Task1/src/main/java/co/gersua/udacity/introhadoop/finalproject/task1/Main.java
@@ -0,0 +1,8 @@
+package co.gersua.udacity.introhadoop.finalproject.task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ FinalProject/Task1/src/main/java/co/gersua/udacity/introhadoop/finalproject/task1/PostingTimeReducer.java
@@ -0,0 +1,15 @@
+package co.gersua.udacity.introhadoop.finalproject.task1;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class PostingTimeReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+    }
+}
